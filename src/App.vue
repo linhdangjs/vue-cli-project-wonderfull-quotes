9b55cc46126ec48eb84913650a5ef316051b95a2
linhdangjs <linhdang.js9896@gmail.com>
2018-11-19 18:34:41 +0700

    finished project wonderful quotes

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,59 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid 
+          :quotes="quotes"
+          @quoteDeleted="deleteQuote">
+    </app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
+  </div>   
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
 export default {
-  name: 'app',
-  data () {
+  data: function() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      quotes: [
+              'Just a Quote to see something'
+      ],
+      maxQuotes: 10
     }
+  },
+  methods: {
+    newQuote(quote) {
+      
+      if(this.quotes.length >= this.maxQuotes) {
+        return alert('Please delete Quotes first!');
+      } else {
+        if(quote != "") {
+        this.quotes.push(quote);
+        } else {
+        return alert('Empty Quote!');
+        }
+      }
+      
+      
+    },
+    deleteQuote(index) {
+        this.quotes.splice(index, 1);
+    }
+  },
+  components : {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style> 
+ 
 </style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-12 col-md-6 m-auto col-md-offset-3 form-group">
+                <label for="">Quote</label>
+                <textarea class="form-control" rows="3" cols="600" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 mt-3 mx-auto col-12 col-md-6 col-md-offset-3 form-group">
+               <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    import Quote from "./Quote.vue"
+    export default {
+        data: function() {
+            return {
+                quote: ''
+            }
+        },
+        methods: {
+            createNew() {
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
